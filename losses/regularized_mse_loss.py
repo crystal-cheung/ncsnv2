bb25e0d656177b69eb914995b027f7a762d129a3
Tiantian Zhang <85858376+crystal-cheung@users.noreply.github.com>
2022-10-13 11:16:02 +0800

    Update and rename mse_loss.py to regularized_mse_loss.py

--- losses/regularized_mse_loss.py
+++ losses/regularized_mse_loss.py
@@ -3,14 +3,15 @@ import torch
 EPS = 1e-2
 esp = 1e-8
 
-class Fidelity_Loss(torch.nn.Module):
+class Regularized_MSE_Loss(torch.nn.Module):
 
     def __init__(self):
-        super(Fidelity_Loss, self).__init__()
+        super(Regularized_MSE_Loss, self).__init__()
 
     def forward(self, p, g):
         g = g.view(-1, 1)
         p = p.view(-1, 1)
         loss = 1 - (torch.sqrt(p * g + esp) + torch.sqrt((1 - p) * (1 - g) + esp))
+        for i in nn.parameters:
 
         return torch.mean(loss)
